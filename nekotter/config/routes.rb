450cdb28455ad4236479228590e9a82f27aae04d
Tatsuya3030 <prelude.wonderfuldream30@gmail.com>
2019-06-10 18:06:58 +0900

    all
    all

---
+++ nekotter/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'tweets' => 'tweets#index' # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'tweets/new' =>'tweets#new'
+  post 'tweets' => 'tweets#create'
+end
